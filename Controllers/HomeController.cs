1b1d302252c768dfe8f53af000b289df6a1ba2f3
Aaron <6kflow9@gmail.com>
2025-03-25 01:52:36 -0500

    IG-C#

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+
+namespace IGRedSocial.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Registro()
+        {
+            return View();
+        }
+
+        public ActionResult Olvide()
+        {
+            return View();
+        }
+    }
+}
